ad5acac5b5b755f948d062a74d8bbddd973c5739
Kenfordovec <Kent720p@gmail.com>
2020-11-09 21:54:49 +0300

    added create questions controller

--- src/Controller/AdminQuestionsController.php
+++ src/Controller/AdminQuestionsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\AnswerRepository;
+use App\Repository\QuestionRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,11 +15,17 @@ class AdminQuestionsController extends AbstractController
     /**
      * @Route("/admin/questions", name="admin_questions")
      */
-    public function index(): Response
+    public function index(QuestionRepository $questionRepository, AnswerRepository $answerRepository, PaginatorInterface $paginator, Request $request): Response
     {
-
+        $questions = $questionRepository->findAllQuestions();
+        $answers = $answerRepository->findAllAnswers();
+        $result = $paginator->paginate(
+            $questions,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 10)
+        );
         return $this->render('admin_questions/index.html.twig', [
-            'controller_name' => 'AdminQuestionsController',
+            'questions' => $result,
         ]);
     }
 }
