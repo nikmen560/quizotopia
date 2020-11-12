3c83b02bffd5fad80d86eaee8bf17fb20ac6da99
Turuto Aleksei <bissmmhunter@gmail.com>
2020-11-12 15:50:08 +0300

    Added initial createQuiz Controller

--- src/Controller/CreateQuizController.php
+++ src/Controller/CreateQuizController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Quiz;
+use App\Entity\QuizQuestion;
 use App\Form\CreateQuizFormType;
 use App\Repository\QuestionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,11 +16,25 @@ class CreateQuizController extends AbstractController
     /**
      * @Route("/create/quiz", name="create_quiz")
      */
-    public function index(QuestionRepository $questionRepository,Request $request): Response
+    public function index(Request $request): Response
     {
         $quiz=new Quiz();
-        $form = $this->createForm(CreateQuizFormType::class, $quiz);
+        $quiz->setName('Testquiz');
+        $em=$this->getDoctrine()->getManager();
+        $form = $this->createForm(CreateQuizFormType::class);
         $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $questions=$form->get('quizQuestions')->getData();
+            foreach ($questions as $question){
+                $quizQuestion=new QuizQuestion();
+                $quizQuestion->setQuestion($question);
+                $quiz->addQuizQuestion($quizQuestion);
+                $em->persist($quizQuestion);
+            }
+            $em->persist($quiz);
+            $em->flush();
+            return $this->redirectToRoute('admin');
+        }
         return $this->render('create_quiz/index.html.twig', [
             'controller_name' => 'CreateQuizController',
             'CreateQuizForm' => $form->createView(),
