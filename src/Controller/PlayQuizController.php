d0c14541de094a10fa25d3ffb123e683e7c2daa1
Turuto Aleksei <bissmmhunter@gmail.com>
2020-11-16 15:49:22 +0300

    added block quiz feature

--- src/Controller/PlayQuizController.php
+++ src/Controller/PlayQuizController.php
@@ -18,35 +18,37 @@ class PlayQuizController extends AbstractController
      */
     public function index($id, QuizRepository $quizRepository,QuizUserRepository $quizUserRepository,UserRepository $userRepository): Response
     {
-        $user=$userRepository->findByUsername($this->getUser()->getUsername());
-        $playingUser=$quizUserRepository->findByUser($user);
-        if($playingUser==null)
-        {
-            $currentTime = new \DateTime();
-            $quiz = $quizRepository->find($id);
-            $firstQuestion = $quiz->getQuizQuestions()[0];
-            $quizUser = new QuizUser();
-            $quizUser->setUser($user);
-            $quizUser->setQuiz($quiz);
-            $quizUser->setResult(0);
-            $quizUser->setCurrentQuestion(0);
-            $quizUser->setStartDate($currentTime);
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($quizUser);
-            $em->flush();
-            $response = $this->forward('App\Controller\QuizPlayerController::index', [
-                'id' => $firstQuestion->getId(),
-                'quizId' => $id,
-                'firstId' => $firstQuestion->getId(),
-            ]);
-            return $response;
+        if($quizRepository->find($id)->getStatus()) {
+            $user = $userRepository->findByUsername($this->getUser()->getUsername());
+            $playingUser = $quizUserRepository->findByUser($user);
+            if ($playingUser == null) {
+                $currentTime = new \DateTime();
+                $quiz = $quizRepository->find($id);
+                $firstQuestion = $quiz->getQuizQuestions()[0];
+                $quizUser = new QuizUser();
+                $quizUser->setUser($user);
+                $quizUser->setQuiz($quiz);
+                $quizUser->setResult(0);
+                $quizUser->setCurrentQuestion(0);
+                $quizUser->setStartDate($currentTime);
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($quizUser);
+                $em->flush();
+                $response = $this->forward('App\Controller\QuizPlayerController::index', [
+                    'id' => $firstQuestion->getId(),
+                    'quizId' => $id,
+                    'firstId' => $firstQuestion->getId(),
+                ]);
+                return $response;
+            } else {
+                return $this->redirectToRoute('quiz_player', [
+                    'id' => $playingUser->getCurrentQuestion(),
+                    'quizId' => $playingUser->getQuiz()->getId()
+                ]);
+            }
         }
-        else
-        {
-            return $this->redirectToRoute('quiz_player',[
-                'id'=>$playingUser->getCurrentQuestion(),
-                'quizId'=>$playingUser->getQuiz()->getId()
-            ]);
+        else{
+            return $this->redirectToRoute('quizzes');
         }
     }
 }
