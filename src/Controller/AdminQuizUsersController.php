72c49951143a6493291018095c7810344c0b7729
Kenfordovec <Kent720p@gmail.com>
2020-11-18 18:27:20 +0300

    created adminQuizUsersController

---
+++ src/Controller/AdminQuizUsersController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\QuizUser;
+use App\Repository\QuizUserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class AdminQuizUsersController extends AbstractController
+{
+    /**
+     * @Route("/admin/users/quiz", name="admin_quiz_users")
+     */
+    public function index(QuizUserRepository $quizUserRepository, Request $request): Response
+    {
+        $users = $quizUserRepository->findAll();
+        return $this->render('admin_quiz_users/index.html.twig', [
+            'users' => $users,
+        ]);
+    }
+}
